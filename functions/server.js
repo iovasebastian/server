896b02597b75d4948cf9dc8e511f6e0c220f1808
iovasebastian <iovasebastian@yahoo.com>
2023-12-18 21:19:53 +0200

    server

--- functions/server.js
+++ functions/server.js
@@ -1,29 +1,23 @@
 // server.js
 const express = require('express');
-const router = express.Router();
 const mongoose = require('mongoose');
-const serverless = require('serverless-http')
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 5001;
 
-app.use(cors({ origin: '*' }));
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-app.use('*',cors());
+app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB Atlas
-
-// Connect to MongoDB using the loaded connection string
 mongoose.connect('mongodb+srv://iovasebastian8:Sebica2003@project.y36dsll.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
+  .then(() => {
+    console.log('MongoDB connected successfully');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
 
 const ItemSchema = new mongoose.Schema({
   question: String,
@@ -31,41 +25,47 @@ const ItemSchema = new mongoose.Schema({
 });
 const Item = mongoose.model('Item', ItemSchema);
 
-app.post('/items', async (req, res) => {
+
+app.get('/api/items', async (req, res) => {
   try {
-    const newItem = new Item(req.body);
-    await newItem.save();
-    res.status(201).json(newItem);
+    const allItems = await Item.find();
+    res.status(200).json(allItems);
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-router.get('/test',(req,res) =>{
-  res.json({
-    'hello':'hi'
-  })
+
+
+app.post('/api/items', async (req, res) => {
+  try {
+    const newItem = new Item(req.body);
+    await newItem.save();
+
+    // Return only the desired fields (e.g., question and answer)
+    const responseItem = {
+      _id: newItem._id,
+      question: newItem.question,
+      answer: newItem.answer,
+    };
+
+    res.status(201).json(responseItem);
+  } catch (error) {
+    console.error('Error saving item:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
-app.get('/items', async (req, res) => {
-    try {
-      const allItems = await Item.find();
-      res.status(200).json(allItems);
-    } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-
-app.delete('/items', async (req, res) => {
-    try {
-      await Item.deleteMany({});
-      res.status(200).json({ message: 'All entries deleted successfully.' });
-    } catch (error) {
-      console.error('Error deleting entries:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-
-
-app.use('/.netlify/functions/server', router);
-module.exports.handler = serverless(app);
+app.delete('/api/items', async (req, res) => {
+  try {
+    await Item.deleteMany({});
+    res.status(200).json({ message: 'All entries deleted successfully.' });
+  } catch (error) {
+    console.error('Error deleting entries:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+const PORT = process.env.PORT || 3000; // Use the provided port or default to 3000
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
 
