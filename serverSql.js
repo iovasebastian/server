18bedccb6c5ced320eb737c594abe60e27cbe790
iovasebastian <iovasebastian@yahoo.com>
2025-11-24 16:47:13 +0100

    db correction 2

--- serverSql.js
+++ serverSql.js
@@ -52,7 +52,7 @@ app.use(cors({
 
 
 const conn = mysql.createPool({
-    host: process.env.SQL_HOST_LOCAL, //change SQL_HOST_LOCAL to SQL_HOST 
+    host: process.env.SQL_HOST, //change SQL_HOST_LOCAL to SQL_HOST 
     user: process.env.SQL_USER,
     password: process.env.SQL_PASSWORD,
     database: process.env.SQL_DATABASE
