592b4ac76f890319f92cd2751775c2845a954e04
iovasebastian <iovasebastian@yahoo.com>
2025-11-24 16:44:00 +0100

    db correction

--- serverSql.js
+++ serverSql.js
@@ -55,8 +55,7 @@ const conn = mysql.createPool({
     host: process.env.SQL_HOST_LOCAL, //change SQL_HOST_LOCAL to SQL_HOST 
     user: process.env.SQL_USER,
     password: process.env.SQL_PASSWORD,
-    database: process.env.SQL_DATABASE,
-    port: process.env.DB_PORT
+    database: process.env.SQL_DATABASE
 });
 
 app.post(
